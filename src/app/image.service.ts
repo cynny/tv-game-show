14648e941a73a2d135da648cfeb367d4a956a86e
cynny <99002017+cynny@users.noreply.github.com>
2022-02-28 01:52:40 -0600

    added more data

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { 
+    getShowImage (showImage: string) {
+      return this.httpClient.get
+      (`https://api.tvmaze.com/shows/:id/images?q=${showImage}&appId=${environment.appId}`)
+    }
+    }
+  }
 }
