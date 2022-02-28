040ca367980b406b6a995a9f411578fca4d8795f
cynny <99002017+cynny@users.noreply.github.com>
2022-02-27 21:51:04 -0600

    created show-image component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { SearchBoxComponent } from './search-box/search-box.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ShowImageComponent } from './show-image/show-image.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchBoxComponent,
+    ShowImageComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule
