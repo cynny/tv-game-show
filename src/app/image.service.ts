a26707b9d744cb1772feda13d1646a140a35074c
cynny <99002017+cynny@users.noreply.github.com>
2022-03-03 02:41:30 -0600

    updated code

--- src/app/image.service.ts
+++ src/app/image.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
-import { stringify } from 'querystring';
+import { IimageData } from './iimage-data';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
 
-  constructor(private httpClient: HttpClient) { 
+  constructor(private httpClient: HttpClient) {}
+
+  getShowDetails(showName: string) {
+    return this.httpClient.get<ImageData>
+    (`https://api.tvmaze.com/singlesearch/shows?q=${showName}`)
 
   }}
