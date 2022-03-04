3a8a73c82510be1ddf3af187b927f5b7c399b24c
SherineVishal <63886367+SherineVishal@users.noreply.github.com>
2022-03-04 10:34:04 -0800

    Rebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,10 @@ import { SearchBoxComponent } from './search-box/search-box.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { CurrentShowComponent } from './current-show/current-show.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBoxComponent,
-    CurrentShowComponent
+    SearchBoxComponent
   ],
   imports: [
     BrowserModule,
