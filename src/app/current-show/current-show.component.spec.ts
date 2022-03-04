d12decb0277d75ff0a0308619d425b4771d54c02
SherineVishal <63886367+SherineVishal@users.noreply.github.com>
2022-03-03 21:05:43 -0800

    Added current-show-component

---
+++ src/app/current-show/current-show.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CurrentShowComponent } from './current-show.component';
+
+describe('CurrentShowComponent', () => {
+  let component: CurrentShowComponent;
+  let fixture: ComponentFixture<CurrentShowComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CurrentShowComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CurrentShowComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
